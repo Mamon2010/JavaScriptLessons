215b51713b6f3e446d0c151a9a5ae262674e2d37
Mamon2010 <ishumilin@list.ru>
2018-11-17 23:18:31 +0300

    Дополнительные задания ко второму уроку

--- lesson 2/additionTask/src/js/script.js
+++ lesson 2/additionTask/src/js/script.js
@@ -1,3 +1,4 @@
+//1. Создать массив week и записать в него дни недели в виде строк
 // let week = new Array("Понедельник", "Вторник", "Среда", "Четверг", "Пятница",
 //     "Суббота", "Воскресение");
 // for (let i = 0; i < week.length; i++) {
@@ -21,3 +22,16 @@
 //             break;
 //     }
 // }
+
+
+//2. Создать массив arr = []
+let arrNum = ['123', '432', '237', '235', '1734', '553', '234'],
+    strNum = arrNum.toString();
+let arrSpecialNum = [];
+
+for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] == 3 || strNum[i] == 7) {
+        arrSpecialNum.push(strNum[i]);
+    }
+}
+console.log(arrSpecialNum);

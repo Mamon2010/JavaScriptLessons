49552f8c280e561dee10c087ec30c45374d4afac
Mamon2010 <ishumilin@list.ru>
2018-11-17 19:53:46 +0300

    Первое дополнительное задание

---
+++ lesson 2/additionTask/src/js/script.js
@@ -0,0 +1,23 @@
+// let week = new Array("Понедельник", "Вторник", "Среда", "Четверг", "Пятница",
+//     "Суббота", "Воскресение");
+// for (let i = 0; i < week.length; i++) {
+//     let res = 'res' + i;
+//     switch (week[i]) {
+//         case 'Суббота':
+//             {
+//                 document.getElementById(res).innerHTML = week[i].bold().italics();
+//             }
+//             break;
+//         case 'Воскресение':
+//             {
+//                 document.getElementById(res).innerHTML = week[i].bold();
+//             }
+//             break;
+
+//         default:
+//             {
+//                 document.getElementById(res).innerHTML = week[i];
+//             }
+//             break;
+//     }
+// }

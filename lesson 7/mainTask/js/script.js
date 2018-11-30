061da2daeaecca48327f34e8de8f88d20ef2a4d4
Mamon2010 <ishumilin@list.ru>
2018-11-30 03:07:56 +0300

    Урок 7. ДЗ - Написать рабочие табы

--- lesson 7/mainTask/js/script.js
+++ lesson 7/mainTask/js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    // Пишем табы по видео
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,4 +36,44 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+});
+
+// пишем табы на фото
+
+let photoWrap = document.querySelector('.wrap'),
+    photoSlider = document.querySelectorAll('.slider-item'),
+    photoNext = document.querySelector('.next'),
+    photoPrev = document.querySelector('.prev'),
+    photoSliderDots = document.querySelector('.slider-dots'),
+    photoDot = document.querySelectorAll('.dot');
+
+function hidePhotoContent(a) {
+    for (let i = a; i < photoSlider.length; i++) {
+        photoSlider[i].classList.remove('show');
+        photoSlider[i].classList.add('hide');
+    }
+}
+
+hidePhotoContent(1);
+
+function showPhotoContent(p) {
+    if (photoSlider[p].classList.contains('hide')) {
+        photoSlider[p].classList.remove('hide');
+        photoSlider[p].classList.add('show');
+    }
+}
+
+photoWrap.addEventListener('click', function(event) {
+    let target = event.target; // область куда нажимаем
+    if (target && target.classList.contains('photoWrap')) {
+        for (let i = 0; i < photoSlider.length; i++) {
+            if (target = photoNext) {
+                hidePhotoContent(0);
+                showPhotoContent(i);
+            } else if (target = photoPrev) {
+                hidePhotoContent(0);
+                showPhotoContent(--i);
+            }
+        }
+    }
 });

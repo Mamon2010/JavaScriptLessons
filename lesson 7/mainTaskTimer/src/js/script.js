f8e6fa16172f33fea5cccffa0ad9aae98d860b90
Mamon2010 <ishumilin@list.ru>
2018-11-30 03:08:36 +0300

    Урок 7 ДЗ - Написать простой таймер

---
+++ lesson 7/mainTaskTimer/src/js/script.js
@@ -0,0 +1,14 @@
+// Простой таймер
+
+function getTimer() {
+    let date = new Date(),
+        hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours(),
+        minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(),
+        seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds(),
+
+        h1 = document.querySelector('.time');
+
+    h1.innerText = hours + ':' + minutes + ':' + seconds;
+}
+
+setInterval(getTimer, 1000);

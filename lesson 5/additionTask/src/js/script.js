0c69685dfbd05f06c8b13caa6f37a8e3b93749c5
Mamon2010 <ishumilin@list.ru>
2018-12-10 01:10:49 +0300

    Урок 5. Дополнительное задание

--- lesson 5/additionTask/src/js/script.js
+++ lesson 5/additionTask/src/js/script.js
@@ -1,10 +1,12 @@
 // 1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
-let date = new Date();
-// document.write(date.toLocaleTimeString() + ' ' + date.toLocaleDateString())
-document.write(date.toLocaleTimeString() + ' ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear());
+let date = new Date(),
+    time = ('0' + date.toLocaleTimeString("ru-RU")).slice(-8);
+document.querySelector('.dateNow').innerHTML = `${time} ${date.toLocaleDateString("ru-RU")}`;
+// console.log(`${time} ${date.toLocaleDateString("ru-RU")}`);
 
 // 2) Напишите функцию, которая будет добавлять 0 перед днями и месяцами, которые состоят из одной цифры(из 1.6 .2018 сделает 01.06 .2018)
-date = date.toLocaleTimeString() + ' ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+// date1 = date1.toLocaleDateString("ru-RU");
+date = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
 getNullInDate(date);
 
 function getNullInDate(date) {
@@ -18,11 +20,8 @@ function getNullInDate(date) {
     return console.log(arr.join('.'));
 }
 
-
 // 3) Напишите функцию, которая выводит на страницу текущий день недели на русском языке(реализацию выбираете сами)
-let day = new Date(),
-    dayInRussian = getdayInRussian(day.getDay());
-document.write('<p>' + dayInRussian);
+let day = new Date();
 
 function getdayInRussian(thisDay) {
     switch (thisDay) {
@@ -54,20 +53,27 @@ function getdayInRussian(thisDay) {
     console.log(thisDay);
     return thisDay;
 }
+let today = getdayInRussian(day.getDay());
+document.querySelector('.today').innerHTML = `Сегодня: ${today}`;
+
 
 // 4) Напишите функцию, которая выводит на страницу разницу 
 // между двумя датами в количестве дней
 
+let result = document.querySelector('.result'),
+    btn = document.querySelector('.btn');
 
-// a = document.querySelectorAll('.input')[0];
-// b = document.querySelectorAll('.input')[1];
+btn.addEventListener('click', function() {
+    let input1 = document.querySelector('.input1').valueAsNumber,
+        input2 = document.querySelector('.input2').valueAsNumber;
+    getDayDiff(input1, input2);
+});
 
-// console.log(a + ' ' + b);
 
-// function getDayDiff(a, b) {
-//     var diff = Math.floor(a.getTime() - b.getTime());
-//     var day = 1000 * 60 * 60 * 24;
-
-//     var days = Math.floor(diff / day);
-//     return days;
-// }
+function getDayDiff(input1, input2) {
+    if (input1 > input2) {
+        result.value = `разница дней: ${Math.floor(input1 - input2) / (1000 * 60 * 60 * 24)}`;
+    } else {
+        alert('Не верный диапазон рассчитываемых дат');
+    }
+}
